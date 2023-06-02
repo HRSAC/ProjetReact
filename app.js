99f7e3e661699ec144720cfc53d8cc08021aeec5
HRSAC <canipelanthony@hotmail.fr>
2023-06-02 19:42:03 +0200

    update/index.html

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const title = React.createElement('h1', {}, 'Bonjour tout le monde')
 
 console.log(title)
 
-ReactDOM.render(title, document.body)
+ReactDOM.render(title, document.querySelector('#app'))
