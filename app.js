bc343a75a072714df9e6bb9c6a98febe69beb05c
HRSAC <canipelanthony@hotmail.fr>
2023-06-02 21:41:21 +0200

    ajout/jsx

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 
 let n = 0 
 
+
+function numberFormat(n) {
+    return n.toString().padStart(2, '0')
+}
+
 function render() {
-    const title = React.createElement('h1', {},
-    'Bonjour tout le monde ',
-    React.createElement('span',{}, n))
+    const title = 
+    <h1>
+        Bonjour les gens
+        <span>{numberFormat(n)}</span>
+    </h1>
 
 ReactDOM.render(title, document.querySelector('#app'))
 }
+
+
+//syntax sans jsx
+// function render() {
+//     const title = React.createElement('h1', {},
+//     'Bonjour tout le monde ',
+//     React.createElement('span',{}, n))
+
+// ReactDOM.render(title, document.querySelector('#app'))
+// }
 
 // function render() {
 //     document.querySelector('#app').innerHTML = '<h1>Bonjour tout le monde <span>'+ n + '</h1>'
@@ -18,4 +35,4 @@ render()
 window.setInterval(() => {
     n++
     render()
-},1000)
+},100)
