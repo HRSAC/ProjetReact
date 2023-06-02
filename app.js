50b789754864cf2a844843acfb567c47594a613f
HRSAC <canipelanthony@hotmail.fr>
2023-06-02 19:59:52 +0200

    ajout/compteur

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const title = React.createElement('h1', {}, 'Bonjour tout le monde')
 
-console.log(title)
+let n = 0 
+
+function render() {
+    const title = React.createElement('h1', {},
+    'Bonjour tout le monde ',
+    React.createElement('span',{}, n))
 
 ReactDOM.render(title, document.querySelector('#app'))
+}
+
+// function render() {
+//     document.querySelector('#app').innerHTML = '<h1>Bonjour tout le monde <span>'+ n + '</h1>'
+// }
+
+render()
+
+window.setInterval(() => {
+    n++
+    render()
+},1000)
