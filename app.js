9c210088821d9cdf5256e67df1355c92f018a951
HRSAC <canipelanthony@hotmail.fr>
2023-06-03 14:04:22 +0200

    add/constructor

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-function Welcome ({name, children}) {
+function WelcomeFunc ({name, children}) {
     return <div>
         <h1>Bonjour {name}</h1>
         <p>
             {children}
         </p>
     </div>
+}
+
+class Welcome extends React.Component {
+
+    constructor (props){
+        super(props)
+        console.log(props)
+    }
+
+    render(){
+        return <div>
+        <h1>Bonjour {this.props.name}</h1>
+        <p>
+            {this.props.children}
+        </p>
+    </div>
+    }
 }
 
 ReactDOM.render(<Welcome name="jean">Bonjour tout le monde </Welcome> , document.querySelector('#app'))
