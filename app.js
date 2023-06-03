a3aeb92c774c90693003bd996b7e7154f98dff49
HRSAC <canipelanthony@hotmail.fr>
2023-06-03 13:51:56 +0200

    update/new_template

--- app.js
+++ app.js
@@ -1,49 +1,5 @@
-
-let n = 0 
-
-
-function numberFormat(n) {
-    return n.toString().padStart(2, '0')
+function Welcome ({name}) {
+    return <h1>Bonjour {name}</h1>
 }
 
-function render() {
-    const items = [
-        n,
-        n*2,
-        n*4
-    ]
-    const lis =  items.map((item, k) => <li key={k}>{item}</li>)
-    const title = 
-    <React.Fragment>
-        <h1 id="title" className="title">
-        Bonjour les gens
-        <span>{numberFormat(n)}</span>
-    </h1>
-    <ul>
-        {lis}
-    </ul>
-    </React.Fragment>
-
-ReactDOM.render(title, document.querySelector('#app'))
-}
-
-
-//syntax sans jsx
-// function render() {
-//     const title = React.createElement('h1', {},
-//     'Bonjour tout le monde ',
-//     React.createElement('span',{}, n))
-
-// ReactDOM.render(title, document.querySelector('#app'))
-// }
-
-// function render() {
-//     document.querySelector('#app').innerHTML = '<h1>Bonjour tout le monde <span>'+ n + '</h1>'
-// }
-
-render()
-
-window.setInterval(() => {
-    n++
-    render()
-},100)
+ReactDOM.render(<Welcome name="jean"/>, document.querySelector('#app'))
