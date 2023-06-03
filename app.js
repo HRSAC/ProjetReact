306a13cd59a6d966f9dc407bb87486cbd509a9cb
HRSAC <canipelanthony@hotmail.fr>
2023-06-03 13:57:47 +0200

    udpate/jsx

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-function Welcome ({name}) {
-    return <h1>Bonjour {name}</h1>
+function Welcome ({name, children}) {
+    return <div>
+        <h1>Bonjour {name}</h1>
+        <p>
+            {children}
+        </p>
+    </div>
 }
 
-ReactDOM.render(<Welcome name="jean"/>, document.querySelector('#app'))
+ReactDOM.render(<Welcome name="jean">Bonjour tout le monde </Welcome> , document.querySelector('#app'))
